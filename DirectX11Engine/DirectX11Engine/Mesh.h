d256c34e5ec75900d8e719422835dbd3978c69fa
Austin Silcox <asilcox123@gmail.com>
2022-02-06 19:36:07 -0500

    Add Transform

--- DirectX11Engine/DirectX11Engine/Mesh.h
+++ DirectX11Engine/DirectX11Engine/Mesh.h
@@ -16,4 +16,28 @@ public:
 protected:
 	std::vector<Vertex> vertices;
 	std::vector<unsigned short> indices;
+
+	struct Transform
+	{
+		struct
+		{
+			float x;
+			float y;
+			float z;
+		} pos;
+
+		struct
+		{
+			float x;
+			float y;
+			float z;
+		} rot;
+
+		struct
+		{
+			float x;
+			float y;
+			float z;
+		} scale;
+	};
 };
